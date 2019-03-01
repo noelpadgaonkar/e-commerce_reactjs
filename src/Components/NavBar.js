875b56eba0e81c62c824d3fd03b316fc38137978
noelpadgaonkar <35414281+noelpadgaonkar@users.noreply.github.com>
2019-03-02 00:17:37 +0530

    Add files via upload

---
+++ src/Components/NavBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../techei 3.png'
+import styled from "styled-components";
+import { ButtonContainer } from "./Button"
+export default class NavBar extends Component {
+	render() {
+		return (
+			<NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+			<Link to='/'>
+			<img src={logo} alt="store"
+			className="navbar-brand" />
+			</Link>
+			<ul className="navbar-nav align-items-center">
+			<li className="nav-item ml-5">
+			<Link to="/" className="nav-link" color="white">
+			Products
+			</Link>
+			</li>
+			</ul>
+			<Link to='/cart' className="ml-auto">
+			<ButtonContainer>
+			<i className="fas fa-cart-plus" />
+			My Cart
+			</ButtonContainer>
+			</Link>
+			</NavWrapper>
+			
+		);
+	}
+}
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link {
+	color: var(--mainWhite) !important;
+	font-size: 1.3rem;
+	text-transform: capitalize;
+}
+`;
